4ffaea8fdb176d57e2d4c11c3540bba87f375e55
Gleb Veksha <veksha.glebka@mail.ru>
2022-11-13 18:23:13 +0300

    Done

--- Lbr6.2/Lbr6.2.cpp
+++ Lbr6.2/Lbr6.2.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
-#include<string>
-#include<regex>
+#include<conio.h>
 using namespace std;
 int main() {
-	string a;
-	regex b("[0-1]+");
-	do {
-		cout << "Fill the line using ones and zeros only\n";
-		getline(cin, a);
-	} while (!regex_match(a, b));
-	regex group("[0-1]{5}");
-	cout << "Number of groups with five digits is ";
-	cout <<distance(sregex_iterator(a.begin(), a.end(), group), sregex_iterator());
+	char string[256];
+	int  x;
+	cout << "Enter only 1 or 0\n";
+	cin.getline(string, 256);
+	int len = strlen(string);
+	for (int i = 0; i < len; i++) {
+		while (string[i] != '1' && string[i] != '0') {
+			cout << "Error. Your string has other elements. You must enter only 1 or 0. Enter it again\n";
+			cin.getline(string, 256);
+			len = strlen(string);
+			
+		}
+	}
+	cout << "Amount of groups with five elements\n";
+	x = (int) strlen(string) / 5;
+	cout << x;
 	return 0;
 }
