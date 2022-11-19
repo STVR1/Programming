ba96def022c809c18c77c3d178cd710277a518a8
Gleb Veksha <veksha.glebka@mail.ru>
2022-11-19 20:51:14 +0300

    I'm the creater

---
+++ Checkingpart/Checkingpart(with char array).cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	bool ping = true;
+	int done = 0;
+	double c = 0;
+	do {
+		char check[256];
+		cout << "Enter number (double or integer)\n";
+		cin.getline(check, 256);
+		bool point = false;
+		for (int i = 0; i < strlen(check); i++) {
+			if (isdigit(check[i])) {
+				done++;
+			}
+			else if ( i != 0  && i != (strlen(check) - 1) && check[i]=='.' && point==false) {
+				done++;
+				point = true;
+			}
+			else if (i==0 && check[i]=='-' && i != (strlen(check) - 1)) {
+				done++;
+			}
+			else {
+				cout << "Error\n";
+				ping = false;
+				break;
+			}
+			if (done == strlen(check)) {
+				ping = true;
+				break;
+			}
+		}
+		c = atof(check);
+		done = 0;
+	} while (ping == false);
+	cout << c;
+	return 0;
+}
---
+++ Checkingpart/Checkingpart(with regex).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<string>
+#include<regex>
+using namespace std;
+int main()
+{
+    string a;
+    regex b("^[-]?([1-9]{1}[0-9]+|[0-9]{1})((.[0-9]+)?)$");
+    cout << "Enter string a\n";
+    getline(cin, a);
+    while (!regex_match(a, b)) {
+        cout << "Error. Input it again\n";
+        getline(cin, a);
+    }
+    double a1 = stod(a);
+    cout << a1;
+    return 0;
+}
